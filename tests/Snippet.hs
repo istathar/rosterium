baeca343e1fe0ea22a3d0f369906f9641822d7b7
Andrew Cowie <andrew@operationaldynamics.com>
2016-05-24 22:30:32 +1000

    Experiment with random-shuffle

--- tests/Snippet.hs
+++ tests/Snippet.hs
@@ -7,11 +7,22 @@ import qualified Data.ByteString.Lazy.Char8 as L
 import Data.Text (Text)
 import qualified Data.Text.IO as T
 import Data.Hourglass
+import System.Random
+import System.Random.Shuffle
 import Time.System
 
 import Rosterium.Setup
 
+
 main :: IO ()
 main = do
+    gen <- newStdGen
+    let input  = [1,2,3,4]
+    let num = length input
+    let result = shuffle' input num gen
+    print result
+
+hourglass :: IO ()
+hourglass = do
     now <- timeCurrentP
     print $ timeGetDateTimeOfDay now
